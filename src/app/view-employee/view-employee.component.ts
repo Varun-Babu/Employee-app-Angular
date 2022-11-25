23e241169671f38c39d158d171d3d29390637efc
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-25 11:09:22 +0530

    data binded

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
+ data=[
+  {
+  "name" :"Varun",
+  "id" : 11,
+  "designation" : "software engineer",
+  "email" : "dadda@gmail.com"
+ },
+ {
+  "name" :"Arun",
+  "id" : 12,
+  "designation" : "software engineer",
+  "email" : "dadda@gmail.com"
+ },
+ {
+  "name" :"Tarun",
+  "id" : 13,
+  "designation" : "software engineer",
+  "email" : "dadda@gmail.com"
+ },
+ {
+  "name" :"Kiran",
+  "id" : 14,
+  "designation" : "software engineer",
+  "email" : "dadda@gmail.com"
+ },
+ {
+  "name" :"Sravan",
+  "id" : 15,
+  "designation" : "software engineer",
+  "email" : "dadda@gmail.com"
+ }
+]
 
 }
